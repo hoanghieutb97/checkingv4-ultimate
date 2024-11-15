41fa6f2266d27721f406de92dd875d6e73d61c90
Hoanghieutb97 <hoanghieutb97@gmail.com>
2024-11-15 19:17:24 +0700

    vsdv

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
-export const GLLM = "https://sheetdb.io/api/v1/xvqhrhg4y9avq";
-// export const GLLM = "https://sheet.best/api/sheets/e8876c80-1778-414d-ae68-af6b9ec1289c";
-// export const SILICON = "https://sheet.best/api/sheets/a6626440-fdcc-41a2-9779-ae2b35a7767a";
+// export const GLLM = "https://sheetdb.io/api/v1/xvqhrhg4y9avq";
+export const GLLM = "https://sheet.best/api/sheets/e8876c80-1778-414d-ae68-af6b9ec1289c";
+
 
 export const PRODUCTS = [
     ["SP Bình Thường",
